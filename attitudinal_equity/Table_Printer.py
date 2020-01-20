c920fbbd577c379ca028f845b221af5eb5e41fd4
jaymeweiss <jaymeweiss@gmail.com>
2020-01-20 16:50:02 +0200

    refactor: pull shared code into reused method

--- attitudinal_equity/Table_Printer.py
+++ attitudinal_equity/Table_Printer.py
@@ -4,33 +4,36 @@ from attitudinal_equity.AE_calculations import calculate_total_ae_for_brand
 
 
 def print_attitudinal_equity_table(responses, number_of_responses, brand_names):
-    table = []
     print('Attitudinal Equity')
+    table = []
     headers = ['Response'] + brand_names + ['Total']
 
     for x in range(number_of_responses):
         response = responses[x]
         row_values = [response.response_id]
-        current_index = 0
         ordered_ratings = sorted(response.ratings, key=lambda y: y.table_index)
+        current_index = 0
         total_attitudinal_equity = 0
         for brand in ordered_ratings:
-            while current_index < brand.table_index:
-                row_values.append('-')
-                current_index += 1
+            current_index = fill_blank_values(current_index, row_values, brand.table_index)
 
             row_values.append("{:10.3f}".format(brand.attitudinal_equity))
             total_attitudinal_equity = total_attitudinal_equity + brand.attitudinal_equity
             current_index += 1
 
-        while current_index < 6:
-            row_values.append('-')
-            current_index += 1
+        fill_blank_values(current_index, row_values, 6)
 
         row_values.append("{:10.3f}".format(total_attitudinal_equity))
         table.append(row_values)
 
     print(tabulate(table, headers))
+
+
+def fill_blank_values(current_index, row_values, upper_bound):
+    while current_index < upper_bound:
+        row_values.append('-')
+        current_index += 1
+    return current_index
 
 
 def print_total_brand_level_ae(responses, brand_names):
