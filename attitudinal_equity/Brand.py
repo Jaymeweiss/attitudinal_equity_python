d8197fcee25b1e0db32f019b49d6f90de7ea17ad
jaymeweiss <jaymeweiss@gmail.com>
2020-01-20 16:20:54 +0200

    chore: correct line spacing

--- attitudinal_equity/Brand.py
+++ attitudinal_equity/Brand.py
@@ -1,6 +1,7 @@
 DEFAULT_RANKING = 0
 DEFAULT_AE = 0
 
+
 class Brand:
     def __init__(self, brand, value, table_index, ranking=DEFAULT_RANKING, attitudinal_equity=DEFAULT_AE):
         self.brand = brand
